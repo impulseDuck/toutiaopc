fe52102f199ea757f73f5b605d7de248be5e220e
impulseDuck <qizi542261886@gmail.com>
2020-02-29 15:42:17 +0800

    初始化项目-准备目录-整理资源

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import '@/styles/index.less'
 import 'element-ui/lib/theme-chalk/index.css' // 引入模块
 Vue.config.productionTip = false // 引入样式
 Vue.use(ElementUI) // 全局注册，一旦全局注册，所有的位置都可以使用
